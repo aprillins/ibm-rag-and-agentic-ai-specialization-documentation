1dd49dc3b508c97450df96fcad795cf5efd3ea52
aprillins <aprillins@gmail.com>
2026-01-16 09:23:10 +0700

    add writer agent and serper websearch tool

--- building-agentic-ai/main.py
+++ building-agentic-ai/main.py
@@ -1,9 +1,14 @@
 from crewai import Crew, Process, Agent, Task, LLM
+from crewai_tools import SerperDevTool
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+
+search_tool = SerperDevTool()
+topic = "Latest advancements in quantum computing"
+search_result = search_tool.run(query=topic)
 llm = LLM(model="gpt-4.1-mini", api_key=openai_api_key)
 
 research_agent = Agent(
@@ -15,7 +20,18 @@ research_agent = Agent(
   verbose=True,
   allow_delegation=False,
   llm = llm,
-  #tools=[SerperDevTool()]
+  tools=[SerperDevTool()]
+)
+
+writer_agent = Agent(
+  role='Tech Content Strategist',
+  goal='Craft well-structured and engaging content based on research findings',
+  backstory="""You are a skilled content strategist known for translating 
+  complex topics into clear and compelling narratives. Your writing makes 
+  information accessible and engaging for a wide audience.""",
+  verbose=True,
+  llm = llm,
+  allow_delegation=True
 )
 
 research_task = Task(
@@ -24,13 +40,19 @@ research_task = Task(
   expected_output="A detailed report on {topic}, including trends, emerging technologies, and their impact."
 )
 
+writer_task = Task(
+  description="Using the research report on {topic}, create an engaging article suitable for publication.",
+  agent=writer_agent,
+  expected_output="A well-structured article on {topic} based on the research findings."
+)
+
 crew = Crew(
     llm=llm,
-    agents=[research_agent],
-    tasks=[research_task],
+    agents=[research_agent, writer_agent],
+    tasks=[research_task, writer_task],
     processes=Process.sequential,
     verbose=True
 )
-result = crew.kickoff(inputs={"topic": "Artificial Intelligence in Healthcare"})
+result = crew.kickoff(inputs={"topic": topic})
 print("Final Result:")
 print(result.raw)
